37f4a8093890ad11594e593e518a1357e9f8d07d
Alin Cosmin <alincosmin123@gmail.com>
2013-12-02 20:34:33 +0200

    Adaugat proiect MVC

---
+++ D10 ASP.NET MVC/MvcApplication1/MvcApplication1/Controllers/EmployeesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        //
+        // GET: /Employees/
+        
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Insert(EmployeesModels employeeModel)
+        //{
+        //    EmployeesModels _emp = new EmployeesModels();
+        //    if (ModelState.IsValid)
+        //    {
+        //        _emp.Name = employeeModel.Name;
+        //        _emp.Salary = employeeModel.Salary;
+        //        EmployeesModels.Add(_emp);
+        //    }
+                
+        //    return View();
+        //}
+
+        public ActionResult View()
+        {
+            List<EmployeesModels> _list = EmployeesModels.GetAll();
+            return View(_list);
+        }
+    }
+}
---
+++ D10 ASP.NET MVC/MvcApplication1/MvcApplication1/Models/EmployeesModels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Models
+{
+    public class EmployeesModels
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Salary")]
+        public double Salary { get; set; }   
+
+        public static List<EmployeesModels> GetAll()
+        {
+            List<EmployeesModels> _list = (List<EmployeesModels>)HttpContext.Current.Session["Employees"];
+            if (_list == null)
+            {
+                _list = new List<EmployeesModels>();
+                HttpContext.Current.Session["Employees"] = _list;
+            }
+            return _list; 
+        }
+
+        public static void Add(EmployeesModels _employee)
+        {
+            List<EmployeesModels> _list = (List<EmployeesModels>)HttpContext.Current.Session["Employees"];
+            _list.Add(_employee);
+            HttpContext.Current.Session["Employees"] = _list;
+        }
+    }
+ 
+}
